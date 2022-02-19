53a80e028aae37232fe26d1c2203108c32c1db55
Hasan F <hfidan006@gmail.com>
2022-02-19 14:36:02 +0100

    us004Update01

--- src/test/java/tests/US_0004/Test01.java
+++ src/test/java/tests/US_0004/Test01.java
@@ -1,8 +1,12 @@
 package tests.US_0004;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.HotelMyCampHotelList;
 import pages.HotelMyCampMain;
 import utilities.ConfigReader;
 import utilities.Driver;
@@ -14,6 +18,7 @@ public class Test01 {
 
 
     HotelMyCampMain hotelMyCamp;
+    HotelMyCampHotelList hotelList;
 
     @Test (priority = 1)
     public void loginButonuDisplayed() throws IOException {
@@ -77,18 +82,54 @@ public class Test01 {
 //adresinde 'Create Hotel' yazisi gorunuyor olmali."
 
     @Test(priority = 5)
-    public void CreateHotelDisplayed() {
+    public void CreateHotelDisplayed() throws IOException {
 
         WebElement createhoteltext = hotelMyCamp.createHotelTextElementi;
         Assert.assertTrue(createhoteltext.isDisplayed());
+
+        ReusableMethods.getScreenshot("Create_Hotel_Displayed");
 
         String expectedText ="Create Hotel";
         String actualtext = createhoteltext.getText();
 
         Assert.assertEquals(actualtext,expectedText);
 
+    }
+
+    @Test(priority = 6)
+    public void hotelAdd01() throws IOException {
+
+       // String code ="13579";
+        hotelList.addHotelCodeBoxElementi.sendKeys("13579");
+
+        String hotelName = "Deneme Hotel";
+        hotelList.addHotelNameBoxElementi.sendKeys(hotelName);
+
+        String hotelAddress ="9 E. Loockerman street, suite 311 Dover, DE 19901";
+        hotelList.addHotelAddressBoxElementi.sendKeys(hotelAddress);
+
+        String hotelPhone="302 718 2180";
+        hotelList.addHotelPhoneBoxElementi.sendKeys(hotelPhone);
+
+        String hotelMail = "info@hotelmycamp.com";
+        hotelList.addHotelEmailBoxElementi.sendKeys(hotelMail);
 
 
+        WebElement dropDownElementi = hotelList.addHotelIDGroupDropDownElementi;
+        Select select = new Select(dropDownElementi);
+        select.selectByIndex(1);
+        Actions actions = new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.END).perform();
+        hotelList.addHotelSaveButtonElementi.click();
+
+        hotelMyCamp.bekle(1);
+        ReusableMethods.getScreenshot("Hotel_Save");
+        String expectedResultText = "Hotel was inserted successfully";
+        String actualResultText = hotelList.addHotelAlertMesajiElementi.getText();
+        Assert.assertTrue(actualResultText.contains(expectedResultText));
+
+        WebElement alertOkButtonElementi = hotelList.addHotelAlertOkButtonElementi;
+        alertOkButtonElementi.click();
     }
 
 
