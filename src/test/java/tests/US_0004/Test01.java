12eb0108f8226f84c39d24a1c304ead4d0eedd98
Hasan F <hfidan006@gmail.com>
2022-02-19 13:28:42 +0100

    us04

---
+++ src/test/java/tests/US_0004/Test01.java
@@ -0,0 +1,46 @@
+package tests.US_0004;
+
+import org.testng.annotations.Test;
+
+public class Test01 {
+
+    //TC0001
+    //Login butonu tiklanarak giris yapilabilmeli.
+    //	Username ve Password box alanina veri girilebilmeli ve Login butonuna tiklanabilmeli.
+    //	"https://www.hotelmycamp.com/Admin/UserAdmin/
+    //Adresindeki 'Hotel List' butonu gorunuyor ve tiklanabiliyor olmali."
+    //
+
+
+    @Test
+    public void test01() {
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
