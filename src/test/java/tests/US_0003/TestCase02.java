20f2e730cff9db3470927e5ae127afb7aa3f120b
Hasan Fidan <94446886+hfidan06@users.noreply.github.com>
2022-02-19 19:04:48 +0100

    enesC (#44)
    * 19.02 us_0003 tc0001
    
    * 19.02
    
    * 19.02
    
    * 19.02
    
    * 19.02 us_0003 tc0001
    
    * 19.02
    
    * 19.02
    
    * 19.02
    
    * 19.02
    
    Co-authored-by: Enes Cingir <enes26cngr@gmail.com>
    Co-authored-by: Your Namemfrknylmz <mfyilmaz2626@gmial.com>
    Co-authored-by: EnesCngr <61951751+EnesCngr@users.noreply.github.com>

--- src/test/java/tests/US_0003/TestCase02.java
+++ src/test/java/tests/US_0003/TestCase02.java
@@ -1,4 +1,26 @@
 package tests.US_0003;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HotelMyCampRegistration;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class TestCase02 {
+    HotelMyCampRegistration hotelMyCampRegistration = new HotelMyCampRegistration();
+    @Test
+    public void testcase02(){
+
+        Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+        hotelMyCampRegistration.loginButonu.click();
+
+        hotelMyCampRegistration.createNewAccButton.click();
+
+        String actualRegisterUrl = Driver.getDriver().getCurrentUrl();
+        String expectedRegisterUrl = "https://www.hotelmycamp.com/Account/Register";
+
+        Assert.assertEquals(actualRegisterUrl,expectedRegisterUrl);
+
+        Driver.closeDriver();
+    }
 }
--- src/test/java/tests/US_0003/TestCase04.java
+++ src/test/java/tests/US_0003/TestCase04.java
@@ -1,4 +1,62 @@
 package tests.US_0003;
 
+import com.github.javafaker.Faker;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HotelMyCampRegistration;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class TestCase04 {
+
+    HotelMyCampRegistration hotelMyCampRegistration = new HotelMyCampRegistration();
+    @Test
+    public void testcase04(){
+
+        Driver.getDriver().get(ConfigReader.getProperty("HMCUrl"));
+        hotelMyCampRegistration.loginButonu.click();
+
+        hotelMyCampRegistration.createNewAccButton.click();
+
+        String actualRegisterUrl = Driver.getDriver().getCurrentUrl();
+        String expectedRegisterUrl = "https://www.hotelmycamp.com/Account/Register";
+
+        Assert.assertEquals(actualRegisterUrl,expectedRegisterUrl);
+        Faker faker = new Faker();
+
+        Assert.assertTrue(hotelMyCampRegistration.registrationFormBaslikYazisi.isDisplayed());
+
+        Assert.assertTrue(hotelMyCampRegistration.usernameTextBox.isDisplayed());
+        hotelMyCampRegistration.usernameTextBox.sendKeys(faker.name().username());
+        Assert.assertTrue(hotelMyCampRegistration.passwordTextBox.isDisplayed());
+        hotelMyCampRegistration.passwordTextBox.sendKeys("Es1234567!");
+        Assert.assertTrue(hotelMyCampRegistration.emailTextBox.isDisplayed());
+        hotelMyCampRegistration.emailTextBox.sendKeys(faker.internet().emailAddress());
+        Assert.assertTrue(hotelMyCampRegistration.fullnameTextBox.isDisplayed());
+        hotelMyCampRegistration.fullnameTextBox.sendKeys(faker.name().fullName());
+        Assert.assertTrue(hotelMyCampRegistration.phoneNoTextBox.isDisplayed());
+        hotelMyCampRegistration.phoneNoTextBox.sendKeys(faker.phoneNumber().cellPhone());
+        Assert.assertTrue(hotelMyCampRegistration.snnTextBox.isDisplayed());
+        hotelMyCampRegistration.snnTextBox.sendKeys(faker.number().digits(9));
+        Assert.assertTrue(hotelMyCampRegistration.drivingLicenseTextBox.isDisplayed());
+        hotelMyCampRegistration.drivingLicenseTextBox.sendKeys(faker.number().digits(9));
+        Assert.assertTrue(hotelMyCampRegistration.countryDropdown.isDisplayed());
+        Select select = new Select(hotelMyCampRegistration.countryDropdown);
+        select.selectByVisibleText("United States");
+        Assert.assertTrue(hotelMyCampRegistration.stateDropdown.isDisplayed());
+        Select select1 = new Select(hotelMyCampRegistration.stateDropdown);
+        select1.selectByVisibleText("Alabama");
+        Assert.assertTrue(hotelMyCampRegistration.addresTextBox.isDisplayed());
+        hotelMyCampRegistration.addresTextBox.sendKeys(faker.address().fullAddress());
+        Assert.assertTrue(hotelMyCampRegistration.workingSectorTextBox.isDisplayed());
+        hotelMyCampRegistration.workingSectorTextBox.sendKeys(faker.job().field());
+        Assert.assertTrue(hotelMyCampRegistration.birthDateTextBox.isDisplayed());
+        hotelMyCampRegistration.birthDateTextBox.sendKeys("22.03.1999");
+        Assert.assertTrue(hotelMyCampRegistration.saveButton.isDisplayed());
+        hotelMyCampRegistration.saveButton.click();
+
+
+        Driver.closeDriver();
+    }
 }
