8c7a2351c3c4ae285b9518848fdc52262af2539f
Hasan F <hfidan006@gmail.com>
2022-02-20 01:26:05 +0100

    TestBaseReportEklendi

--- src/test/java/utilities/TestBaseRapor.java
+++ src/test/java/utilities/TestBaseRapor.java
@@ -16,6 +16,8 @@ public abstract class TestBaseRapor {
     protected static ExtentReports extentReports; //extent report'a ilk atamayi yapar
     protected static ExtentTest extentTest; // test pass veya failed gibi bilgileri kaydeder. Ayrica ekran resmi icin de kullaniriz
     protected static ExtentHtmlReporter extentHtmlReporter; // Html raporu duzenler
+
+
     // Test işlemine başlamadan hemen önce (test methodundan önce değil, tüm test işleminden önce)
     @BeforeTest(alwaysRun = true) // alwaysRun : her zaman çalıştır.
     public void setUpTest() {
@@ -33,6 +35,8 @@ public abstract class TestBaseRapor {
         extentHtmlReporter.config().setDocumentTitle("Son Test");
         extentHtmlReporter.config().setReportName("TestNG Report");
     }
+
+
     // Her test methodundan sonra eğer testte hata varsa, ekran görüntüsü alıp rapora ekliyor
     @AfterMethod(alwaysRun = true)
     public void tearDownMethod(ITestResult result) throws IOException {
@@ -46,6 +50,8 @@ public abstract class TestBaseRapor {
         }
         Driver.closeDriver();
     }
+
+
     // Raporlandırmayı sonlandırmak icin
     @AfterTest(alwaysRun = true)
     public void tearDownTest() {
